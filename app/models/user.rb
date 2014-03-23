27b56e6390c6ea63e7a979a0cec6f93e07094030
Pavel Kovalyov <sxe.funky@gmail.com>
2014-03-23 22:39:00 +0200

    Close #1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 50}
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true,
+                    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ },
+                    uniqueness: { case_sensitive: false }
 
   before_save { email.downcase! }
 
   has_secure_password
   validates :password, length: { minimum: 6 }
+
 end
