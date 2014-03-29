c6b14d7273575e6d9232e9a66f8a292575237dff
Pavel Kovalyov <sxe.funky@gmail.com>
2014-03-29 21:58:13 +0200

    Remove user custom validation in prior to Devise Update layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :name, presence: true, length: {maximum: 50}
-  validates :email, presence: true,
-                    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ },
-                    uniqueness: { case_sensitive: false }
-
-  before_save { email.downcase! }
-
-  has_secure_password
-  validates :password, length: { minimum: 6 }
 
   has_and_belongs_to_many :projects
   has_many :tasks
