109c1508424928b74e0d21ec06eecfa5a62b4f98
Roman Lemekha <er@roleme.net>
2014-03-30 14:40:03 -0400

    projects contoller

---
+++ spec/controllers/projects_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+class SomeControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+  def setup
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in FactoryGirl.create(:user)
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do |u|
+    u.name { Faker::Name.first_name }
+    u.email {Faker::Internet.email}
+  end
+end
