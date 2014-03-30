b9538d2e8aa57d6de5913f621c56345bc0db6b25
Roman Lemekha <er@roleme.net>
2014-03-30 15:05:46 -0400

    project controller test added

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,10 +1,59 @@
 require 'spec_helper'
 
-class SomeControllerTest < ActionController::TestCase
+describe ProjectsController do
   include Devise::TestHelpers
 
-  def setup
-    @request.env["devise.mapping"] = Devise.mappings[:user]
+  before(:each) do
     sign_in FactoryGirl.create(:user)
   end
+  
+  describe "GET #show" do
+    it "assigns the requested project to @project" do
+      project = FactoryGirl.create(:project)
+      get :show, id: project
+      assigns(:project).should eq(project)
+    end
+    it "renders the #show view" do
+      get :show, id: FactoryGirl.create(:project)
+      response.should render_template :show
+    end
+  end
+  
+  describe "GET #index" do
+    it "renders the :index view" do
+      get :index
+      response.should render_template :index
+    end
+  end
+  
+  describe "GET #show" do
+    it "assigns the requested project to @project" do
+      project = FactoryGirl.create(:project)
+      get :show, id: project
+      assigns(:project).should eq(project)
+    end
+    
+    it "renders the #show view" do
+      get :show, id: FactoryGirl.create(:project)
+      response.should render_template :show
+    end
+  end
+  
+  describe 'DELETE destroy' do
+    before :each do
+      @project= FactoryGirl.create(:project)
+    end
+    
+    it "deletes the project" do
+      expect{
+        delete :destroy, id: @project
+      }.to change(Project,:count).by(-1)
+    end
+      
+    it "redirects to projects#index" do
+      delete :destroy, id: @project
+      response.should redirect_to projects_url
+    end
+  end
+  
 end
--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -2,5 +2,11 @@ FactoryGirl.define do
   factory :user do |u|
     u.name { Faker::Name.first_name }
     u.email {Faker::Internet.email}
+    u.password {'password'}
+  end
+  
+  factory :project do |p|
+    p.name { 'name' }
+    p.description {'description'}
   end
 end
