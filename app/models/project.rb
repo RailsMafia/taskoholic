58b666b6950357ced47ad9a05e87b6b2cbd9372f
Roman Lemekha <er@roleme.net>
2014-04-02 16:04:11 -0400

    project validations. add model test

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,5 +4,8 @@ class Project < ActiveRecord::Base
   belongs_to :owner,
     class_name: "User"
 
-  validates :name, presence: true
+  validates_associated :owner
+  validates :name, presence: true, length: {minimum: 2, maximum: 50}
+  validates :owner, presence: true
+  validates :description, length: {minimum: 10, maximum: 1000}
 end
