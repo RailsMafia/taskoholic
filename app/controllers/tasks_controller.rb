18932aa2723e66211765411f4c3613e1835b0036
Sergey Zhuga <sergey.zhuga@gmail.com>
2014-04-10 22:49:48 +0300

    updated tasks controller tests for existing methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,7 +27,7 @@ class TasksController < ApplicationController
   end
 
   def update
-    if @task.update(project_params)
+    if @task.update(task_params)
       redirect_to project_tasks_url, notice: "Task \"#{@task.name}\" was successfully updated."
     else
       render 'edit'
